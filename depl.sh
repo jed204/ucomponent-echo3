0fd6742f5388812ef315686162c460f5e945bbed
jed204 <jed.danner@gmail.com>
2012-07-24 13:16:50 -0400

    Initial import

--- depl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cp output/UntzUntzComponents.jar ../Application/jbpm-server/server/jbpm/lib/UntzUntzComponents.jar 
-cp output/UntzUntzComponents.jar ../EvenFlow/lib/UntzUntzComponents.jar 
-
-echo done
